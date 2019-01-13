5bd8a8b782b4d0392db2168116d7662ed6d41a5f
Chris Delli Santi <chris.dellisantijr@gmail.com>
2019-01-13 15:27:52 -0500

    Fixed force-close due to hanging runnable

--- app/src/main/java/com/demo/chris/newscorpdemo/ui/main/SplashFragment.kt
+++ app/src/main/java/com/demo/chris/newscorpdemo/ui/main/SplashFragment.kt
@@ -7,10 +7,34 @@ import com.demo.chris.newscorpdemo.R
 import com.nochino.support.androidui.fragments.BaseSplashFragment
 
 class SplashFragment : BaseSplashFragment() {
+
+    // TODO :: Move navigation responsibility to BaseSplashFragment
+    private var nextFragmentRunnable: Runnable? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.postDelayed({
+
+        // TODO :: Move navigation responsibility to BaseSplashFragment
+        nextFragmentRunnable = Runnable {
             findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
-        }, 1500)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // TODO :: Move navigation responsibility to BaseSplashFragment
+        view?.postDelayed(nextFragmentRunnable, 1500)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // TODO :: Move navigation responsibility to BaseSplashFragment
+        view?.removeCallbacks(nextFragmentRunnable)
+    }
+
+    override fun onDestroyView() {
+        // TODO :: Move navigation responsibility to BaseSplashFragment
+        nextFragmentRunnable = null
+        super.onDestroyView()
     }
 }
