1b8dd783c866ca34ca80b67037444f00ed5ee763
Chris Delli Santi <chris.dellisantijr@gmail.com>
2019-02-03 23:46:38 -0500

    Nothing of impact here...Code cleanup

--- app/src/main/java/com/demo/chris/newscorpdemo/ui/main/SplashFragment.kt
+++ app/src/main/java/com/demo/chris/newscorpdemo/ui/main/SplashFragment.kt
@@ -1,35 +1,31 @@
 package com.demo.chris.newscorpdemo.ui.main
 
-import android.os.Bundle
-import android.view.View
 import androidx.navigation.fragment.findNavController
 import com.demo.chris.newscorpdemo.R
 import com.nochino.support.androidui.fragments.BaseSplashFragment
 
 class SplashFragment : BaseSplashFragment() {
 
-    // TODO :: Move navigation responsibility to BaseSplashFragment
-    private var nextFragmentRunnable: Runnable? = null
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        nextFragmentRunnable = Runnable {
-            findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
-        }
+    private var nextFragmentRunnable: Runnable? = Runnable {
+        findNavController().navigate(R.id.action_splashFragment_to_mainFragment)
     }
 
     override fun onResume() {
         super.onResume()
+        // After 1.5 seconds go to the next fragment
         view?.postDelayed(nextFragmentRunnable, 1500)
     }
 
     override fun onPause() {
         super.onPause()
+
+        // If the fragment is paused before the runnable can execute
+        // remove it.
         view?.removeCallbacks(nextFragmentRunnable)
     }
 
     override fun onDestroyView() {
+        // Clean-up!
         nextFragmentRunnable = null
         super.onDestroyView()
     }
