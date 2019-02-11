fd8791373bb359fae216fc7c15155975cd1ab0ac
Chris Delli Santi <chris.dellisantijr@gmail.com>
2019-02-10 21:07:51 -0500

    Moved Timber initializtion to lib_androidui Application class

--- app/src/main/java/com/demo/chris/newscorpdemo/NewsCorpDemoApplication.kt
+++ app/src/main/java/com/demo/chris/newscorpdemo/NewsCorpDemoApplication.kt
@@ -7,7 +7,6 @@ import com.nochino.support.networking.util.MutableLiveDataCallAdapterFactory
 import com.nochino.support.networking.RetroNetWorker
 import com.nochino.support.networking.execution.AppExecutors
 import retrofit2.converter.gson.GsonConverterFactory
-import timber.log.Timber
 
 class NewsCorpDemoApplication: AndroidUIApplication() {
 
@@ -42,9 +41,6 @@ class NewsCorpDemoApplication: AndroidUIApplication() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-
-        // TODO :: WIP :: Create Crash Reporting Tree
-        Timber.plant(Timber.DebugTree())
     }
 
     companion object {
